5217a510945f2b5d40136c2f6dfb17e00272cb97
armando <armandohernandez0194@gmail.com>
2018-11-26 19:43:53 -0700

    Added pix2pix model

--- src/pose_extraction.py
+++ src/pose_extraction.py
@@ -14,42 +14,6 @@ import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 # tf.logging.set_verbosity(tf.logging.ERROR)
 
-parser = argparse.ArgumentParser(description='')
-
-parser.add_argument('--source_video', default='source.mp4', help='Source video name')
-parser.add_argument('--source_path', default='../data/source/', help='Path where the source video is saved')
-parser.add_argument('--target_video', default='target.mp4', help='target video name')
-parser.add_argument('--target_path', default='../data/target/', help='Path where the target video is saved')
-parser.add_argument('--frames', default=100000, type=int, help='Number of frames to process')
-parser.add_argument('--open_pose_weights', type=str, default='../model/openpose/model.h5', help='path to the weights file')
-parser.add_argument('--seed', type=str, default=None, help='Seed')
-parser.add_argument('--p2p_output_dir', type=str, default='../model/pix2pix/', help='path to the weights file')
-parser.add_argument("--scale_size", type=int, default=286, help="scale images to this size before cropping to 256x256")
-parser.add_argument("--separable_conv", action="store_true", help="use separable convolutions in the generator")
-parser.add_argument("--flip", dest="flip", action="store_true", help="flip images horizontally")
-parser.add_argument("--no_flip", dest="flip", action="store_false", help="don't flip images horizontally")
-parser.set_defaults(flip=True)
-parser.add_argument("--batch_size", type=int, default=1, help="number of images in batch")
-parser.add_argument("--lr", type=float, default=0.0002, help="initial learning rate for adam")
-parser.add_argument("--beta1", type=float, default=0.5, help="momentum term of adam")
-parser.add_argument("--l1_weight", type=float, default=100.0, help="weight on L1 term for generator gradient")
-parser.add_argument("--gan_weight", type=float, default=1.0, help="weight on GAN term for generator gradient")
-parser.add_argument("--ngf", type=int, default=64, help="number of generator filters in first conv layer")
-parser.add_argument("--ndf", type=int, default=64, help="number of discriminator filters in first conv layer")
-parser.add_argument("--max_steps", type=int, help="number of training steps (0 to disable)")
-parser.add_argument("--max_epochs", type=int, help="number of training epochs")
-parser.add_argument("--summary_freq", type=int, default=100, help="update summaries every summary_freq steps")
-parser.add_argument("--progress_freq", type=int, default=50, help="display progress every progress_freq steps")
-parser.add_argument("--trace_freq", type=int, default=0, help="trace execution every trace_freq steps")
-parser.add_argument("--display_freq", type=int, default=0, help="write current training images every display_freq steps")
-parser.add_argument("--save_freq", type=int, default=5000, help="save model every save_freq steps, 0 to disable")
-parser.add_argument("--aspect_ratio", type=float, default=1.0, help="aspect ratio of output images (width/height)")
-parser.add_argument("--lab_colorization", action="store_true", help="split input image into brightness (A) and color (B)")
-parser.add_argument("--checkpoint", default=None, help="directory with checkpoint to resume training from or use for testing")
-
-
-args = parser.parse_args()
-
 def extract_frames_from_video(source_path, source_video, frames):
     """
     Extracts the frames of vieo and saves it in 
@@ -127,9 +91,44 @@ def main(args):
     # generate_labels(args.target_path, args.open_pose_weights, max_frames, 'train_images')
     model = Pix2PixModel(args)
     model.train()
-    print('end')
+
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+
+    parser.add_argument('--source_video', default='source.mp4', help='Source video name')
+    parser.add_argument('--source_path', default='../data/source/', help='Path where the source video is saved')
+    parser.add_argument('--target_video', default='target.mp4', help='target video name')
+    parser.add_argument('--target_path', default='../data/target/', help='Path where the target video is saved')
+    parser.add_argument('--frames', default=100000, type=int, help='Number of frames to process')
+    parser.add_argument('--open_pose_weights', type=str, default='../model/openpose/model.h5', help='path to the weights file')
+    parser.add_argument('--seed', type=str, default=None, help='Seed')
+    parser.add_argument('--p2p_output_dir', type=str, default='../model/pix2pix/', help='path to the weights file')
+    parser.add_argument("--scale_size", type=int, default=286, help="scale images to this size before cropping to 256x256")
+    parser.add_argument("--separable_conv", action="store_true", help="use separable convolutions in the generator")
+    parser.add_argument("--flip", dest="flip", action="store_true", help="flip images horizontally")
+    parser.add_argument("--no_flip", dest="flip", action="store_false", help="don't flip images horizontally")
+    parser.set_defaults(flip=True)
+    parser.add_argument("--batch_size", type=int, default=1, help="number of images in batch")
+    parser.add_argument("--lr", type=float, default=0.0002, help="initial learning rate for adam")
+    parser.add_argument("--beta1", type=float, default=0.5, help="momentum term of adam")
+    parser.add_argument("--l1_weight", type=float, default=100.0, help="weight on L1 term for generator gradient")
+    parser.add_argument("--gan_weight", type=float, default=1.0, help="weight on GAN term for generator gradient")
+    parser.add_argument("--ngf", type=int, default=64, help="number of generator filters in first conv layer")
+    parser.add_argument("--ndf", type=int, default=64, help="number of discriminator filters in first conv layer")
+    parser.add_argument("--max_steps", type=int, help="number of training steps (0 to disable)")
+    parser.add_argument("--max_epochs", type=int, help="number of training epochs")
+    parser.add_argument("--summary_freq", type=int, default=100, help="update summaries every summary_freq steps")
+    parser.add_argument("--progress_freq", type=int, default=50, help="display progress every progress_freq steps")
+    parser.add_argument("--trace_freq", type=int, default=0, help="trace execution every trace_freq steps")
+    parser.add_argument("--display_freq", type=int, default=0, help="write current training images every display_freq steps")
+    parser.add_argument("--save_freq", type=int, default=5000, help="save model every save_freq steps, 0 to disable")
+    parser.add_argument("--aspect_ratio", type=float, default=1.0, help="aspect ratio of output images (width/height)")
+    parser.add_argument("--lab_colorization", action="store_true", help="split input image into brightness (A) and color (B)")
+    parser.add_argument("--checkpoint", default=None, help="directory with checkpoint to resume training from or use for testing")
+    parser.add_argument("--which_direction", type=str, default="BtoA", choices=["AtoB", "BtoA"])
+
+
+    args = parser.parse_args()
     main(args)
 
-    
